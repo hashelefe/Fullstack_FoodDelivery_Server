c200f7dbebdf1426e16b1532bb72abf10f5525ba
Jakub Nowaczek <j.nowaczek30@gmail.com>
2023-12-05 22:09:26 +0100

    Deployment Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:5000',
     'http://localhost:3000',
-    'https://fullstackfooddeliveryapp-production.up.railway.app/'
+    'https://fooddelivery-app-0yu5.onrender.com/'
 ];
 
 module.exports = allowedOrigins;
