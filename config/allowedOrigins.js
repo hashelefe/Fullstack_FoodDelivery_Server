60681f3b9b66808f94da2c8a43f4bf2cb9e05a9c
Jakub Nowaczek <j.nowaczek30@gmail.com>
2023-12-05 22:20:46 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,5 @@
 const allowedOrigins = [
-    'http://127.0.0.1:3000',
-    'http://localhost:5000',
-    'http://localhost:3000',
-    'https://fooddelivery-app-0yu5.onrender.com/'
+    'https://fooddelivery-app-0yu5.onrender.com'
 ];
 
 module.exports = allowedOrigins;
