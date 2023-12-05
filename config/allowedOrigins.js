9912d828584eb80bb491e4171c577d7f14919e24
Jakub Nowaczek <j.nowaczek30@gmail.com>
2023-12-05 15:47:36 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:5000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://fullstackfooddeliveryapp-production.up.railway.app/'
 ];
 
 module.exports = allowedOrigins;
